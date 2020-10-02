34d52cfb1bda4c91ea84eb0321d927630bd6d3bf
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-02 20:51:00 +0100

    we got headers bitch

--- php/header.php
+++ php/header.php
@@ -1,13 +1,13 @@
 <?php
 
 $logo_id = get_theme_mod( 'custom_logo' );
-$logo = wp_get_attachment_image_src( $logo_id , 'full' );
+$logo = wp_get_attachment_image_src( $logo_id , 'full' )[0];
 
 ?>
 
 <div id="header">
 
-    <img id="logo" href="<?php echo $logo[0]; ?>">
+    <img id="logo" src="<?php echo $logo; ?>">
     <div class="add"></div>
 
 </div>
