85d61af833ec812ec47faa6a8a29d4c6687fe742
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-05 00:06:53 +0100

    bug fix

--- php/functions.php
+++ php/functions.php
@@ -4,12 +4,11 @@ function create_database($name, $columns) {
 
     global $wpdb;
     $name = $wpdb->prefix . $name;
-    $charset_collate = $wpdb->get_charset_collate();
 
     $columns_str = "";
     foreach( $columns as $column ) { $columns_str .= "$column,"; }
 
-    $sql = "CREATE TABLE IF NOT EXISTS $name ( $columns_str ) $charset_collate ; ";
+    $sql = "CREATE TABLE IF NOT EXISTS $name ( $columns_str ) ; ";
 
     require_once(ABSPATH . "wp-admin/includes/upgrade.php");
     dbDelta($sql);
