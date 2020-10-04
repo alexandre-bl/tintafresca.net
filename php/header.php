a93058ee67a91ad352abfb9c87a1330f95889aaf
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-04 22:44:51 +0100

    test

--- php/header.php
+++ php/header.php
@@ -1,13 +1,13 @@
 <?php
 
-//require_once get_template_directory()."/php/functions.php";
+require_once get_template_directory()."/php/functions.php";
 
 $logo_id = get_theme_mod( 'custom_logo' );
 $logo = wp_get_attachment_image_src( $logo_id , 'full' )[0];
 
 $add = "";
 
-create_database("adds", [ "img TEXT", "url TEXT" ]);
+//create_database("adds", [ "img TEXT", "url TEXT" ]);
 
 ?>
 
