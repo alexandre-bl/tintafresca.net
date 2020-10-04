1262140c994bc2bbabbe30993e8bfc3cf1dc988c
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-04 20:31:52 +0100

    start implementing adds

--- php/header.php
+++ php/header.php
@@ -3,11 +3,13 @@
 $logo_id = get_theme_mod( 'custom_logo' );
 $logo = wp_get_attachment_image_src( $logo_id , 'full' )[0];
 
+$add = "";
+
 ?>
 
 <div id="header">
 
     <img id="logo" src="<?php echo $logo; ?>">
-    <div class="add"></div>
+    <div class="add"><img src="<?php echo $add; ?>"></div>
 
 </div>
