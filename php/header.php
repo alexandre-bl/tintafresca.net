95a9a846232540dbf1a96d4a4c3092a249c581a8
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-11 17:05:18 +0100

    who needs functions anyway

--- php/header.php
+++ php/header.php
@@ -8,15 +8,24 @@ print_r(get_table("adds"));
 $logo_id = get_theme_mod( 'custom_logo' );
 $logo = wp_get_attachment_image_src( $logo_id , 'full' )[0];
 
-$add = "";
+global $wpdb;
+$table_name = "adds";
+$table_name = $wpdb->prefix . $table_name;
+$charset_collate = $wpdb->get_charset_collate();
+require_once(ABSPATH . "wp-admin/includes/upgrade.php");
 
-create_table("adds", "img TEXT, url TEXT");
+dbDelta(" CREATE TABLE IF NOT EXISTS $table_name ( name TEXT, img TEXT, url TEXT ) $charset_collate; ");
+
+$add = array( 
+    "img" => $wpdb->get_results(" SELECT img FROM $table_name WHERE name='top'; "),
+    "url" => $wpdb->get_results(" SELECT url FROM $table_name WHERE name='top'; ")
+);
 
 ?>
 
 <div id="header">
 
     <!--<img id="logo" src="<?php //echo $logo; ?>">-->
-    <div class="add"><img src="<?php echo $add; ?>"></div>
+    <div class="add"> <a href="<?php echo $add["url"]; ?>"> <img src="<?php echo $add["img"]; ?>"> </a> </div>
 
 </div>
