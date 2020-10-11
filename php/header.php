62291e1a960f57ac4776974f98090bbe4f15cb35
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-11 17:06:18 +0100

    bug fix

--- php/header.php
+++ php/header.php
@@ -1,8 +1,5 @@
 <?php
 
-set_table("test", "adds", "img", 1);
-print_r(get_table("adds"));
-
 $logo_id = get_theme_mod( 'custom_logo' );
 $logo = wp_get_attachment_image_src( $logo_id , 'full' )[0];
 
