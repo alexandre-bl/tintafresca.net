bbab3f7e8f5f55b364041b9e97cc562ff72a0434
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-11 18:10:15 +0100

    pages

--- php/left_bar.php
+++ php/left_bar.php
@@ -1,6 +1,23 @@
 <?php
 
+$pages = get_pages();
+$pages_html = "";
+
+for( $page as $pages ) {
+
+    $page_link = get_page_link( $page->ID );
+    $page_title = $page->post_title;
+
+    $pages_html .= "<li><a href='$page_link'>$page_title</a></li>";
+
+}
+
 ?>
 
 <div id="left_bar">
+
+    <ul>
+        <?php echo $pages_html; ?>
+    </ul>
+
 </div>
