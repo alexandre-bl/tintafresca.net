8a1906a329f90651db555ad3bc8c385728d55669
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-11 23:01:40 +0100

    client is being an asshole

--- php/center.php
+++ php/center.php
@@ -1,9 +1,15 @@
 <div id="center">
 
     <div class="post" id="st_post">
-        <h1 class="post_title"></h1>
+        <h1 class="post_title">Casos ativos de COVID-19 na Região Oeste triplicam num mês</h1>
         <img class="post_img">
-        <p class="post_desc"></p>
+        <p class="post_desc">
+    O número de casos ativos de COVID-19 na Região Oeste triplicou num mês, passando de cerca de 150 no início de setembro para 423, no dia 10 de outubro. Os concelhos que integram o distrito de Lisboa concentram a grande maioria dos casos, com destaque para Alenquer (112), Torres Vedras (82), Arruda dos Vinhos (49), Sobral de Monte Agraço (24) e Lourinhã (20), a que se junta agora Peniche (57). Na zona norte, com menor número de infeções, Alcobaça continua a ser o concelho mais afetado (38), seguido de Caldas da Rainha (13), Bombarral (12), Cadaval (8), Nazaré (7) e Óbidos (1).
+
+    O número total de infeções aproxima-se dos dois milhares (1949), o número de recuperados é de 1476 e o número de óbitos atingiu a meia centena. Torres Vedras regista já 14 óbitos, seguido de Alcobaça (8), Caldas da Rainha (6), Alenquer (5), Peniche (4), Lourinhã (3), Arruda dos Vinhos (3), Cadaval (2), Sobral de Monte Agraço (2), Nazaré (2) e Bombarral (1).
+
+    O aumento de casos na Região Oeste acompanha o crescimento de infeções em Portugal, que registou este sábado, um máximo absoluto diário de 1646 casos de COVID-19.
+        </p>
     </div>
 
     <div class="post" id="nd_post">
