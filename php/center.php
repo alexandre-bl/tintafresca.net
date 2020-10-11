67f2e2451dd3b9c2e236426e71593674148bb199
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-11 22:38:50 +0100

    client is being an asshole

--- php/center.php
+++ php/center.php
@@ -1,6 +1,25 @@
 <div id="center">
 
     <div id="main_posts">
+
+        <div class="post" id="st_post">
+            <h1 class="post_title"></h1>
+            <img class="post_img">
+            <p class="post_desc"></p>
+        </div>
+
+        <div class="post" id="nd_post">
+            <h1 class="post_title"></h1>
+            <img class="post_img">
+            <p class="post_desc"></p>
+        </div>
+
+        <div class="post" id="trd_post">
+            <h1 class="post_title"></h1>
+            <img class="post_img">
+            <p class="post_desc"></p>
+        </div>
+
     </div>
 
     <div id="sec_posts">
