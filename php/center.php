3e5ab638de80b57e2f643f54077c1cda70eed609
Alexandre B. L <alexandrebl@protonmail.com>
2020-10-18 18:59:22 +0100

    client is an asshole

--- php/center.php
+++ php/center.php
@@ -6,7 +6,7 @@ function get_post_thumbnail($post) {
 
         return "<h1 class='post_title'>$post->post_title</h1>
                 <img class='post_img' src='" . wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0] . "'>
-                <p class='post_desc'>$post->post_content</p>";
+                <div class='post_desc'>$post->post_content</div>";
 
     }
 
