20e65cc33e7415f21b6372bff03247e43e6f5fa4
Alexandre B. L <alexandrebl@protonmail.com>
2020-11-15 20:15:28 +0000

    client is being less of an asshole

--- php/header.php
+++ php/header.php
@@ -9,11 +9,11 @@ $table_name = $wpdb->prefix . $table_name;
 $charset_collate = $wpdb->get_charset_collate();
 require_once(ABSPATH . "wp-admin/includes/upgrade.php");
 
-dbDelta(" CREATE TABLE IF NOT EXISTS $table_name ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, img TEXT, link TEXT ) $charset_collate; ");
+dbDelta(" CREATE TABLE IF NOT EXISTS $table_name ( img TEXT, link TEXT ) $charset_collate; ");
 
 $add = array( 
-    "img" => $wpdb->get_results(" SELECT `img` FROM $table_name WHERE `id` = 0; ")[0]->img,
-    "url" => $wpdb->get_results(" SELECT `link` FROM $table_name WHERE `id` = 0; ")[0]->link
+    "img" => $wpdb->get_results(" SELECT * FROM $table_name ")[0]->img,
+    "url" => $wpdb->get_results(" SELECT * FROM $table_name ")[0]->link
 );
 
 ?>
