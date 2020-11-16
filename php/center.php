36cad0a8526bfc9183dde31028b9e3f362b64b30
Alexandre B. L <alexandrebl@protonmail.com>
2020-11-16 20:45:25 +0000

    make so you can actualy view posts

--- php/center.php
+++ php/center.php
@@ -1,6 +1,8 @@
+<div id="center"
+
 <?php
 
-function get_post_thumbnail($post) {
+function get_post_content($post) {
 
     if( !empty($post->ID) ) {
 
@@ -11,86 +13,97 @@ function get_post_thumbnail($post) {
     }
 
 }
-
-$destaques = $posts = get_posts( array(
-    'numberposts' => 3,
-    "category" =>  get_cat_ID("Destaques")
-  )
-);
-
-for( $i = 0; $i < 3; $i += 1 ) {
-
-    if( empty( $destaques[$i] ) ) {
-
-        $destaques[$i] = null;
-
-    }
-
 }
 
-$posts = get_posts( array(
-    'numberposts' => -1
-  )
-);
+if( is_singular() ) {
 
-$posts_markup = array(  "st"    => get_post_thumbnail($destaques[0]),
-                        "nd"    => get_post_thumbnail($destaques[1]),
-                        "trd"   => get_post_thumbnail($destaques[2]),
-                        "left"  => "",
-                        "right" => ""
-                    );
+	echo ' class="singular">';
 
-$destaques_n = 0;
+	echo get_post_content( get_post() )
 
-for( $i = 0; $i < count($posts); $i += 1 ) {
+} else {
 
-    if( $posts[$i] != $destaques[0] and
-        $posts[$i] != $destaques[1] and
-        $posts[$i] != $destaques[2] )   {
+	echo '>';
 
-        $thumbnail = "<div class='post'>". get_post_thumbnail($posts[$i]) ."</div>";
+	$destaques = $posts = get_posts( array(
+	    'numberposts' => 3,
+	    "category" =>  get_cat_ID("Destaques")
+	  )
+	);
 
-        if( ( $i - $destaques_n ) % 2 == 0 ) {
+	for( $i = 0; $i < 3; $i += 1 ) {
 
-            $posts_markup["left"] .= $thumbnail;
+	    if( empty( $destaques[$i] ) ) {
 
-        } else {
+	        $destaques[$i] = null;
 
-            $posts_markup["right"] .= $thumbnail;
+	    }
 
-        }
+	}
 
-    } else {
+	$posts = get_posts( array(
+	    'numberposts' => -1
+	  )
+	);
+	
+	$posts_markup = array(  "st"    => get_post_content($destaques[0]),
+	                        "nd"    => get_post_content($destaques[1]),
+	                        "trd"   => get_post_content($destaques[2]),
+	                        "left"  => "",
+	                        "right" => ""
+	                    );
+	
+	$destaques_n = 0;
+	
+	for( $i = 0; $i < count($posts); $i += 1 ) {
+	
+	    if( $posts[$i] != $destaques[0] and
+	        $posts[$i] != $destaques[1] and
+	        $posts[$i] != $destaques[2] )   {
+	
+	        $thumbnail = "<div class='post'>". get_post_content($posts[$i]) ."</div>";
+	
+	        if( ( $i - $destaques_n ) % 2 == 0 ) {
+	
+	            $posts_markup["left"] .= $thumbnail;
+	
+	        } else {
+	
+	            $posts_markup["right"] .= $thumbnail;
+	
+	        }
+	
+	    } else {
+	
+	        $destaques_n += 1;
+	
+	    }
+	
+	}	
 
-        $destaques_n += 1;
+	?>
 
-    }
+	    <div class="post main" id="st_post">
+	        <?php echo $posts_markup["st"]; ?>
+	    </div>
 
-}
+	    <div class="post main" id="nd_post">
+	        <?php echo $posts_markup["nd"]; ?>
+	    </div>
+	
+	    <div class="post main" id="trd_post">
+	        <?php echo $posts_markup["trd"]; ?>
+	    </div>
 
-?>
+	    <div id="sec_posts">
+	        <div id="left_posts">
+	            <?php echo $posts_markup["left"]; ?>
+	        </div>
+	        <div id="right_posts">
+	            <?php echo $posts_markup["right"]; ?>
+	        </div>
+	    </div>
 
-<div id="center">
-
-    <div class="post main" id="st_post">
-        <?php echo $posts_markup["st"]; ?>
-    </div>
-
-    <div class="post main" id="nd_post">
-        <?php echo $posts_markup["nd"]; ?>
-    </div>
-
-    <div class="post main" id="trd_post">
-        <?php echo $posts_markup["trd"]; ?>
-    </div>
-
-    <div id="sec_posts">
-        <div id="left_posts">
-            <?php echo $posts_markup["left"]; ?>
-        </div>
-        <div id="right_posts">
-            <?php echo $posts_markup["right"]; ?>
-        </div>
-    </div>
+	<?php } ?>
 
 </div>
