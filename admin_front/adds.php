75a0e175f3e5ae1d1bb494f2fce8909b62c2e172
Alexandre B. L <alexandrebl@protonmail.com>
2020-11-16 22:52:32 +0000

    creating a tool to update the adds

---
+++ admin_front/adds.php
@@ -0,0 +1,23 @@
+<?
+
+function content() {
+
+
+    
+}
+
+add_action( "admin_menu", "create_page");
+
+function create_page() {
+
+    add_menu_page( 
+        "Anúncios",
+        "Anúncios",
+        "manage_options",
+        "anuncios",
+        "anuncios",
+        "content",
+        24
+    );
+
+}
