aede8d6b49731a25a147b2572eee936f2013c697
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-01 21:04:10 +0000

    crunching

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
         <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/style.css" ?>">
         <meta charset="utf-8">
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QRE6N0L516"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QRE6N0L516');
+        </script>
+
     </head>
 
     <body> <?php
