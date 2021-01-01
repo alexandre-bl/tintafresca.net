3c785f564a21d9983a51794f0c1c591712b28b97
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-01 18:33:10 +0000

    featured images possible fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <body> <?php
             
-        require_once "site_front/header.php"; 
+        //require_once "site_front/header.php"; 
         require_once "site_front/left_bar.php";
         require_once "site_front/right_bar.php";
         require_once "site_front/center.php";
