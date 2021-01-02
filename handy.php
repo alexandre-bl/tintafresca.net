0fa35cc997ff153c44988830228b3fee1e1b14d0
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-02 18:42:00 +0000

    nobody reads these

--- handy.php
+++ handy.php
@@ -16,7 +16,7 @@ function get_post_content($post, $singular=FALSE, $opinion=FALSE ) {
 
         if( !$opinion ) {
 
-            $desc = get_post_meta( $post->ID, "Description" )
+            $desc = get_post_meta( $post->ID, "Description" );
 
             if( !empty( $desc ) ) {
 
