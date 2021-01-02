13ded8cf0b95a69d3447c38c0da49851d71961cd
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-02 15:13:45 +0000

    crunching

--- handy.php
+++ handy.php
@@ -6,14 +6,15 @@ function get_post_content($post, $singular=FALSE) {
 
 		$o = "";
 
-		if( !$singular ) {
+        $o .= "<h1 class='post_title'><a href='". get_post_permalink( $post->ID ) ."'>$post->post_title</a></h1>";
+        
+        if( !$singular ) {
 
         	$o .= "<img class='post_img' src='" . wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0] . "'>";
 			
 		}
 
-		$o .= "<h1 class='post_title'><a href='". get_post_permalink( $post->ID ) ."'>$post->post_title</a></h1>
-                <div class='post_desc'>". apply_filters( 'the_content',  $post->post_content ) ."</div>";
+        $o .= "<div class='post_desc'>". apply_filters( 'the_content',  $post->post_content ) ."</div>";
 
 		return $o;
 
