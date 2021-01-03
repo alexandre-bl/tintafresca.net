295dc25e95cea3c87a1e0a303445c5702511cd5f
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-03 14:54:37 +0000

    nobody reads these

--- site_front/sub_header.php
+++ site_front/sub_header.php
@@ -1,11 +1,16 @@
 <?php
 
-$editor = "Mario Lopes";
+$edition = 123;
+$editor  = "Mario Lopes";
+$date    =  get_date();
+$date    =  get_date()["mday"] . "/" . get_date()["month"] . "/" . get_date()["year"];
 
 ?>
 
 <div id="sub-header">
 
+    <p id="edition"><?php echo $edition; ?></p>
     <p id="editor"><?php echo $editor; ?></p>
+    <p id="date"><?php echo $date; ?></p>
 
 </div>
