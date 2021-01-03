19d859760da8439978b5967f5c01ffa1d1089fe7
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-03 15:11:11 +0000

    nobody reads these

--- site_front/sub_header.php
+++ site_front/sub_header.php
@@ -1,6 +1,6 @@
 <?php
 
-$editor  = "Mario Lopes";
+$editor  = get_users( [ 'role__in' => [ 'editor' ] ] )[0]->display_name;
 $date    = getdate();
 $edition = $date["year"]*12 + $date["mon"] - 24011;
 $date    = $date["mday"] . "/" . $date["mon"] . "/" . $date["year"];
