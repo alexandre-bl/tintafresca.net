c22b3044e22c4e0e3d0ed5963d8a013da8646526
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-09 14:32:10 +0000

    09/01/2021 14:32:10

--- meta/test.sh
+++ meta/test.sh
@@ -1,2 +1,2 @@
-git stage -A && git commit -m "$1" && git push && \
+git stage -A && git commit -m "$(date '+%d/%m/%Y %H:%M:%S')" && git push && \
 ssh alex@alexandrebl.com "cd /var/www/tinta/wp-content/themes/tintafresca_wp_theme && git pull"
--- meta/update.sh
+++ meta/update.sh
@@ -1,2 +1,2 @@
-git stage -A && git commit -m "$1" && git push ; \
+./meta/test.sh ; \
 ssh alex@alexandrebl.com "cd /var/www/tintafresca/wp-content/themes/tintafresca_wp_theme && git pull"
