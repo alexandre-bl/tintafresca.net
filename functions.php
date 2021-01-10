f6ff422cb2595ff5c2f24975f9b33d2514f41b1d
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-10 18:37:12 +0000

    10/01/2021 18:37:12

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 <?php
 
-@ini_set( 'upload_max_size' , '512M' );
-@ini_set( 'post_max_size', '512M');
-@ini_set( 'max_execution_time', '300' );
-
 add_theme_support( 'custom-logo' );
 add_theme_support( 'post-thumbnails' );
 
