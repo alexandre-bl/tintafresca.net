4e25d57f319fa34f9feb0b42d96325a20df1a515
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-16 15:22:27 +0000

    16/01/2021 15:22:27

--- meta/test.sh
+++ meta/test.sh
@@ -1,2 +1,2 @@
-git stage -A && git commit -m "$(date '+%d/%m/%Y %H:%M:%S')" && git push ; \
+git stage -A && git commit -m "$(date '+%d/%m/%Y %H:%M:%S')" ; git push ; \
 ssh root@alexandrebl.com "cd /var/www/teste.tintafresca.net/wp-content/themes/tintafresca_wp_theme && git pull"
