ef84cc99c6a5904f02b64a2455fb17dc2bafd4c0
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-16 16:48:10 +0000

    16/01/2021 16:48:10

--- scripts/image_size.js
+++ scripts/image_size.js
@@ -1,18 +1,22 @@
-function image_size(img, w, h) {
+function image_size(img, wp, hp, tp, m) {
+    let parent = img.parentElement;
     let width = img.offsetWidth;
     let height = img.offsetHeight;
+    let p_width = img.offsetWidth;
+    let p_height = parent.offsetHeight;
+    let top = parent.offsetTop;
 
     if (height >= width) {
         img.style.height = "100%";
-        img.style.maxHeight = w;
+        img.style.maxHeight = (p_height - top - m) * hp;
     } else {
         img.style.width = "100%";
-        img.style.maxWidth = h;
+        img.style.maxWidth = (p_width - top - m) * wp;
     }
 }
 
 var imgs = document.getElementsByClassName("post_img");
 
 for (var i = 0; i < imgs.length; i++) {
-    image_size(imgs[i], "60%", "60%");
+    image_size(imgs[i], 0.6, 0.6, 0.5, 5);
 }
