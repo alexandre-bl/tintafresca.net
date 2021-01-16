2691c5fc41a7eed3ea576ce9307ef85e7e94a2f2
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-16 16:28:06 +0000

    16/01/2021 16:28:06

--- scripts/image_size.js
+++ scripts/image_size.js
@@ -8,10 +8,10 @@ function image_size(img, wp, hp, tp, m) {
 
     if (height >= width) {
         img.style.height = "100%";
-        img.style.maxHeight = (p_height - top - margin) * hp;
+        img.style.maxHeight = (p_height - top - m) * hp;
     } else {
         img.style.width = "100%";
-        img.style.maxWidth = (p_width - top - margin) * wp;
+        img.style.maxWidth = (p_width - top - m) * wp;
     }
 }
 
