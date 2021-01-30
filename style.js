d50c4a8c7698f68e324132f3f48cdfbc4951dda4
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-30 18:03:20 +0000

    30/01/2021 18:03:20

--- style.js
+++ style.js
@@ -24,6 +24,7 @@ for( var l = 0; l < descs.length; l++ ) {
     var top = descs[l].offsetTop;
     var height = window.getComputedStyle(descs[l].parentElement, null).getPropertyValue("height");
 
+    console.log(height - top);
     descs[l].style.height = height - top;
 
 }
