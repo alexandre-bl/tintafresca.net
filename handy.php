ce847d19e25c48e1f2839bb5a8878642ef2ed710
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-31 18:09:43 +0000

    31/01/2021 18:09:43

--- handy.php
+++ handy.php
@@ -15,29 +15,26 @@ function get_post_content( $post, $singular=FALSE, $opinion=FALSE ) {
         }
 
         $o .= "<h1 class='post_title'><a href='". get_post_permalink( $post->ID ) ."'>$post->post_title</a></h1>";
+        
+        if( !$singular ) {
+
+        	$o .= "<img class='post_img' src='" . wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0] . "'>";
+			
+		}
 
         if( !$opinion ) {
 
             $desc = get_post_meta( $post->ID, "Description", TRUE );
 
-
-            if( !$singular ) {
-
-                $o .= "<div class='post_desc'><img class='post_img' src='" . wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0] . "'>";
-                
-            }
-
             if( empty( $desc ) or $singular ) {
 
-                $o .= apply_filters( 'the_content',  $post->post_content );
+                $o .= "<div class='post_desc'>". apply_filters( 'the_content',  $post->post_content ) ."</div>";
 
             } else {
 
-                $o .= apply_filters( 'the_content',  $desc );
+                $o .= "<div class='post_desc'>". apply_filters( 'the_content',  $desc ) ."</div>";
 
             }
-
-            $o .= "</div>";
 
         } elseif( !empty(wp_get_attachment_caption( get_post_thumbnail_id( $post->ID ) )) ) {
 
