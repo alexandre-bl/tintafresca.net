625bbe7b656333858601511c889f91796ea2e231
Alexandre B. L <alexandrebl@protonmail.com>
2021-01-31 17:01:52 +0000

    31/01/2021 17:01:52

--- style.js
+++ style.js
@@ -40,6 +40,28 @@ for( var l = 0; l < descs.length; l++ ) {
     y = y.toString().replace("px","");
     height = height.toString().replace("px","");
 
-    descs[l].style.height = height - y + 8 + "px"; 
+    var pos_height = height - y + 8;
 
+    if( descs[l].tagName == "img" ) {
+
+        var width = window.getComputedStyle(descs[l], null).getPropertyValue("width");
+        width = width.toString().replace("px","");
+        var h = window.getComputedStyle(descs[l], null).getPropertyValue("height");
+        h = h.toString().replace("px","");
+        var p_width = window.getComputedStyle(descs[l].parentElement, null).getPropertyValue("width");
+        p_width = p_width.toString().replace("px","");
+
+        var pos_width = width * ( pos_height / h );
+
+        if( pos_width > p_width*0.5 ) {
+
+            descs[l].style.width = p_width*0.5 + "px"; 
+
+        }
+
+    } else {
+
+        descs[l].style.height = pos_height + "px"; 
+
+    }
 }
