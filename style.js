e18c4ca115b65837a40689e1000883779a86600c
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-01 17:02:53 +0000

    01/02/2021 17:02:53

--- style.js
+++ style.js
@@ -1,20 +1,24 @@
-var posts = document.getElementsByClassName("post");
-var to_resize = [];
+function sizing() {
 
-for( var i = 0; i < posts.length; i++ ) {
+    var posts = document.getElementsByClassName("post");
+    var to_resize = [];
 
-    if( posts[i].parentElement.id != "opinion" ) {
+    for( var i = 0; i < posts.length; i++ ) {
 
-        var children = posts[i].children;
+        if( posts[i].parentElement.id != "opinion" ) {
 
-        for( var j = 0; j < children.length; j++ ) {
+            var children = posts[i].children;
 
-            var child = children[j];
+            for( var j = 0; j < children.length; j++ ) {
 
-            if( child.classList.contains("post_desc") ||
-                child.classList.contains("post_img")  ){
+                var child = children[j];
 
-                to_resize.push( child );
+                if( child.classList.contains("post_desc") ||
+                    child.classList.contains("post_img")  ){
+
+                    to_resize.push( child );
+
+                }
 
             }
 
@@ -22,55 +26,57 @@ for( var i = 0; i < posts.length; i++ ) {
 
     }
 
-}
+    for( var l = 0; l < to_resize.length; l++ ) {
 
-for( var l = 0; l < to_resize.length; l++ ) {
+        if( to_resize[l].parentElement.id ) {
 
-    if( to_resize[l].parentElement.id ) {
+            var y = to_resize[l].offsetTop;
 
-        var y = to_resize[l].offsetTop;
+        } else {
 
-    } else {
+            var y = to_resize[l].offsetTop - to_resize[l].parentElement.offsetTop;
 
-        var y = to_resize[l].offsetTop - to_resize[l].parentElement.offsetTop;
+        }
 
-    }
+        var parent_h = window.getComputedStyle(to_resize[l].parentElement, null).getPropertyValue("height");
 
-    var parent_h = window.getComputedStyle(to_resize[l].parentElement, null).getPropertyValue("height");
+        y = y.toString().replace("px","");
+        parent_h = parent_h.toString().replace("px","");
 
-    y = y.toString().replace("px","");
-    parent_h = parent_h.toString().replace("px","");
+        var pos_h = parent_h - y + 8;
 
-    var pos_h = parent_h - y + 8;
+        if( to_resize[l].tagName == "IMG" ) {
 
-    if( to_resize[l].tagName == "IMG" ) {
+            var w = window.getComputedStyle(to_resize[l], null).getPropertyValue("width");
+            w = w.toString().replace("px","");
 
-        var w = window.getComputedStyle(to_resize[l], null).getPropertyValue("width");
-        w = w.toString().replace("px","");
+            var h = window.getComputedStyle(to_resize[l], null).getPropertyValue("height");
+            h = h.toString().replace("px","");
 
-        var h = window.getComputedStyle(to_resize[l], null).getPropertyValue("height");
-        h = h.toString().replace("px","");
+            var parent_w = window.getComputedStyle(to_resize[l].parentElement, null).getPropertyValue("width");
+            parent_w = parent_w.toString().replace("px","");
 
-        var parent_w = window.getComputedStyle(to_resize[l].parentElement, null).getPropertyValue("width");
-        parent_w = parent_w.toString().replace("px","");
+            var pos_w = w * ( pos_h / h );
 
-        var pos_w = w * ( pos_h / h );
+            console.log(w + ", " + h);
 
-        console.log(w + ", " + h);
+            if( pos_w > parent_w*0.5 ) {
 
-        if( pos_w > parent_w*0.5 ) {
+                to_resize[l].style.width = parent_w*0.5 + "px"; 
 
-            to_resize[l].style.width = parent_w*0.5 + "px"; 
+            } else {
+
+                to_resize[l].style.height = pos_h + "px"; 
+
+            }
 
         } else {
 
             to_resize[l].style.height = pos_h + "px"; 
 
         }
-
-    } else {
-
-        to_resize[l].style.height = pos_h + "px"; 
-
     }
+
 }
+
+sizing();
