c445194f55d3f418f28afa258e78dc29e53d3f5d
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-02 17:46:27 +0000

    02/02/2021 17:46:27

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
             require_once "site_front/left_bar.php";
             require_once "site_front/right_bar.php";
             require_once "site_front/center.php";
+            require_once "site_front/comments.php";
             
         ?>
 
