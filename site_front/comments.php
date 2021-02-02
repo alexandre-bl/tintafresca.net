a4304a6ddfc6f50c046e67ff8f420a610ab16fb4
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-02 17:49:45 +0000

    02/02/2021 17:49:44

--- site_front/comments.php
+++ site_front/comments.php
@@ -1,19 +1,21 @@
-<?php
+<div id="comments"> <?php
 
-//Get only the approved comments
-$args = array(
-    'status' => 'approve'
-);
- 
-// The comment Query
-$comments_query = new WP_Comment_Query;
-$comments = $comments_query->query( $args );
- 
-// Comment Loop
-if ( $comments ) {
- foreach ( $comments as $comment ) {
- echo '<p>' . $comment->comment_content . '</p>';
- }
-} else {
- echo 'No comments found.';
-}
+    //Get only the approved comments
+    $args = array(
+        'status' => 'approve'
+    );
+    
+    // The comment Query
+    $comments_query = new WP_Comment_Query;
+    $comments = $comments_query->query( $args );
+    
+    // Comment Loop
+    if ( $comments ) {
+        foreach ( $comments as $comment ) {
+            echo '<p>' . $comment->comment_content . '</p>';
+        }
+    } else {
+        echo 'No comments found.';
+    }
+
+?> </div>
