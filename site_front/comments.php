b94f928fc5275549ab22f2e7caa90bb618face18
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-03 16:15:36 +0000

    03/02/2021 16:15:36

--- site_front/comments.php
+++ site_front/comments.php
@@ -1,14 +1,9 @@
 <div id="comments"> <?php
 
-    //Get only the approved comments
     $args = array(
-        'status' => 'approve',
-        'avatar_size' => 74
+        'post_id' => get_post()->ID
     );
-    
-    // The comment Query
-    $comments_query = new WP_Comment_Query;
-    $comments = $comments_query->query( $args );
+    $comments = get_comments( $args );
     
     echo "<h1>Comentários:</h1>";
 
