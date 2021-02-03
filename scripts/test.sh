c2394c31cd80854a8288fc10e5c5cd4461ab2388
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-03 15:09:13 +0000

    03/02/2021 15:09:13

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 git stage -A && git commit -m "$(date '+%d/%m/%Y %H:%M:%S')" ; git push ; \
-ssh root@tintafresca.net "cd /var/www/tintafresca/test/wp-content/themes/tintafresca.net && git pull"
+ssh root@tintafresca.net "cd /var/www/teste.tintafresca/wp-content/themes/tintafresca.net && git pull"
