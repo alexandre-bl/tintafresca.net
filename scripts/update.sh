c88ce4d4655ab6469d949d6cb94a0141c5c2ba53
Alexandre B. L <alexandrebl@protonmail.com>
2021-02-20 13:48:50 +0000

    20/02/2021 13:48:50

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,2 +1,2 @@
 ./scripts/test.sh ; \
-ssh root@tintafresca.net "cd /var/www/tintafresca/wp-content/themes/tintafresca.net && git pull"
+ssh tintafresca.net "cd /var/www/tintafresca/wp-content/themes/tintafresca.net && git pull"
